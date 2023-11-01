36e91dde9a14b44b7f8e9122c114df23b12dcb73
iancdesponds <iancdesponds@gmail.com>
2023-11-01 16:36:09 -0300

    Basic Login

---
+++ Main.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import requests
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+def register():
+    st.header("Cadastro de Usuário")
+    username = st.text_input("Usuário ")
+    if st.button("Cadastrar"):
+        response = requests.post('http://localhost:5000/register', data={'username': username})
+        if response.status_code == 201:
+            st.success("Cadastro realizado com sucesso. Faça o login.")
+        else:
+            st.error("Erro no cadastro. Tente novamente.")
+
+def login():
+    st.header("Login")
+    username = st.text_input("Usuário")
+    if st.button("Login"):
+        response = requests.post('http://localhost:5000/login', data={'username': username})
+        if response.status_code == 200:
+            st.success("Login bem-sucedido.")
+        else:
+            st.error("Usuário não encontrado. Tente novamente.")
+
+
+def login_register():
+    tab1, tab2 = st.tabs(["Login", "Cadastro"])
+    with tab1:
+        login()
+    with tab2:
+        register()
+
+if __name__ == '__main__':
+    login_register()
---
+++ pages/Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import requests
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+st.title("Home")
+st.write("Bem-vindo!")
