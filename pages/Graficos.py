ac457b5d3835d5ac824e8ba4deb4be282b70fe71
iancdesponds <iancdesponds@gmail.com>
2023-11-04 20:24:50 -0300

    Added sidebar pages

--- pages/Graficos.py
+++ pages/Graficos.py
@@ -1,2 +1,17 @@
 import streamlit as st
 import requests
+from st_pages import Page, show_pages, add_page_title
+from streamlit_extras.switch_page_button import switch_page
+
+st.set_page_config(initial_sidebar_state="collapsed", layout="wide", page_title="Gráficos")
+
+show_pages(
+    [
+        Page("pages/Menu.py", "Home"),
+        Page("pages/Produtos.py", "Produtos"),
+        Page("pages/Estoque.py", "Estoque"),
+        Page("pages/Graficos.py", "Gráficos"),
+    ]
+)
+
+st.title("Gráficos")
