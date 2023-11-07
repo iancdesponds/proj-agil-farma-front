64895e298788cf2925f4171d7e8b2a66415f3a4f
iancdesponds <iancdesponds@gmail.com>
2023-11-07 04:02:58 -0300

    Gráficos de vendas

--- pages/Vendas.py
+++ pages/Vendas.py
@@ -4,6 +4,9 @@ from st_pages import Page, show_pages, add_page_title
 from streamlit_extras.switch_page_button import switch_page
 from time import sleep
 import pandas as pd
+import scipy
+import plotly.figure_factory as ff
+import plotly.express as px
 
 st.set_page_config(initial_sidebar_state="collapsed", layout="wide", page_title="Vendas")
 
@@ -111,6 +114,73 @@ def vendas_realizadas():
 
 def graficos():
     st.header("Gráficos")
+    data = requests.get('http://127.0.0.1:5000/vendas').json()
+    df = pd.DataFrame(data["Vendas"])
+    if df.empty:
+        st.warning("Não há vendas realizadas.")
+    else:
+        df = df[["Produto", "Quantidade", "Data de Validade", "Fornecedor", "Custo por Unidade", "Preço de Venda", "Data da Venda"]]
+        df["Valor Total"] = df["Quantidade"] * df["Preço de Venda"]
+        df["Custo total"] = df["Quantidade"] * df["Custo por Unidade"]
+        df["Lucro"] = df["Valor Total"] - df["Quantidade"] * df["Custo por Unidade"]
+
+        df_receita = df[["Lucro", "Valor Total", "Custo total", "Produto", "Data da Venda"]]
+        
+        df_receita['Data da Venda'] = pd.to_datetime(df_receita['Data da Venda'], format='%Y-%m-%d')
+
+        df_receita['Ano'] = df_receita['Data da Venda'].dt.year
+        df_receita['Mês'] = df_receita['Data da Venda'].dt.month
+
+        df_mensal = df_receita.groupby(['Ano', 'Mês']).agg({'Lucro': 'sum', 'Custo total': 'sum', 'Valor Total': 'sum'}).reset_index()
+
+        df_mensal = df_mensal.rename(columns={'Ano': 'Ano', 'Mês': 'Mês', 'Lucro': 'Lucro Mensal', 'Custo total': 'Custo Total Mensal', 'Valor Total': 'Valor Total Mensal'})
+
+        df_mensal['Data Mensal'] = pd.to_datetime(df_mensal['Ano'].astype(str) + '-' + df_mensal['Mês'].astype(str), format='%Y-%m')
+          
+        
+        # Gráfico de barras com custo total, preço total e lucro por dia
+        fig1 = px.bar(df_receita, x="Data da Venda", y=["Lucro", "Valor Total", "Custo total"], title="Lucro, Valor Total e Custo Total por dia (R$)", labels= {"value": "Valor (R$)", "variable": "Tipo de valor"})
+        st.plotly_chart(fig1, use_container_width=True)
+
+        col1, col2 = st.columns(2, gap='large')
+        with col1:
+            # Gráfico de pizza com lucro por produto
+            fig2 = px.pie(df, values='Lucro', names='Produto', title='Lucro por produto (R$)')
+            st.plotly_chart(fig2, use_container_width=False)
+        
+        
+        with col2:
+            # Gráfico de barras com lucro mensal
+            fig3 = px.bar(df_mensal, x="Data Mensal", y="Lucro Mensal", title='Lucro mensal (R$)', labels= {"value": "Valor (R$)", "variable": "Tipo de valor"})
+            fig3.update_xaxes(
+                tickformat="%b - %Y",  # Formato para "Mês - Ano"
+                showgrid=True  # Exibe as grades no eixo x
+            )
+            fig3.update_layout(bargap=0.8)
+            st.plotly_chart(fig3, use_container_width=False)
+
+        col1, col2 = st.columns(2, gap='large')
+        with col1:
+            # Gráfico de barras com valor total mensal
+            fig4 = px.bar(df_mensal, x="Data Mensal", y="Valor Total Mensal", title='Valor Total mensal (Dos produtos vendidos) (R$)', labels= {"value": "Valor (R$)", "variable": "Tipo de valor"})
+            fig4.update_xaxes(
+                tickformat="%b - %Y",  # Formato para "Mês - Ano"
+                showgrid=True  # Exibe as grades no eixo x
+            )
+            fig4.update_layout(bargap=0.8)
+            st.plotly_chart(fig4, use_container_width=True)
+
+        with col2:
+            # Gráfico de barras com custo total mensal
+                fig5 = px.bar(df_mensal, x="Data Mensal", y="Custo Total Mensal", title='Custo Total mensal (Dos produtos vendidos) (R$)', labels= {"value": "Valor (R$)", "variable": "Tipo de valor"})
+                fig5.update_xaxes(
+                    tickformat="%b - %Y",  # Formato para "Mês - Ano"
+                    showgrid=True  # Exibe as grades no eixo x
+                )
+                fig5.update_layout(bargap=0.8)
+                st.plotly_chart(fig5, use_container_width=True)
+
+    
 
 def tabs():
     tab1, tab2, tab3 = st.tabs(["Registrar Venda", "Vendas Realizadas", "Gráficos"])
